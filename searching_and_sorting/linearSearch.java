12e5742378ce75ac8b5a43342ce73744957c057a
mdakram09 <iamakramkhan09@gmail.com>
2022-05-05 10:27:43 +0530

    2 linear binary search and bubble sort is done

--- searching_and_sorting/linearSearch.java
+++ searching_and_sorting/linearSearch.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class linearSearch {
     public static void main(String[] args) {
-        System.out.println("hello");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int arr[] = new int[n];
+        System.out.println("enter element to find");
+        int x = s.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == x) {
+                System.out.println(x + " is present at " + i + "th index");
+            }
+        }
     }
 }
